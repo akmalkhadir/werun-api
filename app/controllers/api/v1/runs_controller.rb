42b269a94c500b82a4000b0c95c3f6d4eae8392a
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-12-18 09:07:00 +0000

    of cors

--- app/controllers/api/v1/runs_controller.rb
+++ app/controllers/api/v1/runs_controller.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class Api::V1::RunsController < ApplicationController
-    before_action :set_run, only: [:show]
-  
+  before_action :set_run, only: [:show]
+
   def index
     @runs = Run.all
     render json: @runs
@@ -10,14 +12,23 @@ class Api::V1::RunsController < ApplicationController
     render json: @run
   end
 
-   private
+  def create
+    @run = Run.new(run_params)
+    if @run.save
+      render json: @run
+    else
+      render json: {error: 'Unable to create a run.'}, status: 400
+    end
+  end
+
+  private
 
   def set_run
     @run = Run.find(params[:id])
   end
 
   def run_params
-    params.require(:run).permit(:id)
+    params.require(:run).permit(:name, :description, :start_location, :end_location, :date, :distance, :private, :runner_id)
   end
 
 end
